eeb3c5b6ce32f619086e6b6a01c0c94eeab1734c
Letícia Cavalcanti <128481031+lcavalcantic@users.noreply.github.com>
2023-08-17 16:07:36 -0300

    Create codepark16.js

---
+++ codepark16.js
@@ -0,0 +1,13 @@
+// criando as listas
+let nomes = ['Leticia', 'Sofia', 'Bento', 'Clara', 'Eduardo', 'Fernanda', 'Gabriel', 'Helena', 'Isabela', 'João'];
+let idades = [25, 30, 22, 28, 34, 27, 29, 31, 24, 26];
+let coresFavoritas = ['azul', 'verde', 'vermelho', 'roxo', 'amarelo', 'laranja', 'rosa', 'preto', 'branco', 'cinza'];
+
+// imprimindo as listas originais
+console.log('Nomes:', nomes);
+console.log('Idades:', idades);
+console.log('Cores Favoritas:', coresFavoritas);
+
+// modificações
+idades[2] = 23; // 
+coresFavoritas[4] = 'azul-marinho';
