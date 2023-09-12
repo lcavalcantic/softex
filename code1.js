7fff97a247eba5c1b113d5db7446e5e2bec91f51
Letícia Cavalcanti <128481031+lcavalcantic@users.noreply.github.com>
2023-09-12 11:38:39 -0300

    Create code1.js

---
+++ code1.js
@@ -0,0 +1,25 @@
+// Declaração das variáveis
+let booleanVar;
+let bigintVar;
+let stringVar;
+let symbolVar;
+
+// Exibindo os conteúdos iniciais (undefined)
+console.log("Conteúdo inicial das variáveis:");
+console.log("booleanVar:", booleanVar);
+console.log("bigintVar:", bigintVar);
+console.log("stringVar:", stringVar);
+console.log("symbolVar:", symbolVar);
+
+// Atribuindo valores nulos às variáveis
+booleanVar = null;
+bigintVar = null;
+stringVar = null;
+symbolVar = null;
+
+// Exibindo as variáveis após atribuir valores nulos
+console.log("\nVariáveis após atribuir valores nulos:");
+console.log("booleanVar:", booleanVar);
+console.log("bigintVar:", bigintVar);
+console.log("stringVar:", stringVar);
+console.log("symbolVar:", symbolVar);
